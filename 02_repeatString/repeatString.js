c71aa157627aaaaa2b2b6f60a4ed08f66352c0cb
Mark Trent <mmtrent@gmail.com>
2023-09-01 16:39:06 -0700

    Complete all removeFromArray tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(inputString, numberOfRepeats) {
+	if (numberOfRepeats == 0) {
+		return("");
+	} else if (numberOfRepeats < 0) {
+		return("ERROR");
+	} else if (numberOfRepeats > 0) {
 
+		let outputString = inputString;
+		for (let i = 1; i < numberOfRepeats; i++) {
+			outputString = inputString.concat(outputString);
+		};
+		return(outputString);
+	}
+	
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
 const removeFromArray = function() {
+    const inputArray = arguments[0];
+
+    for (let i = 1; i < arguments.length; i++) {
+        let indexToRemove = inputArray.indexOf(arguments[i]);
+
+        // indexOf returns -1 if the element is not found
+        if (indexToRemove !== -1) {
+            // use splice to remove the element from the array if it was found by indexOf
+            inputArray.splice(indexToRemove, 1);
+        }
+    }
+    return(inputArray);
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
